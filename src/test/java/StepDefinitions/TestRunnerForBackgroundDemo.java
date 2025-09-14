eaf5dab4cd1d77d5d5654750c0cac3e73d6ec8fc
vedantmarne <vedantno.1@gmail.com>
2025-09-14 22:30:52 +0530

    Initial commit

--- src/test/java/StepDefinitions/TestRunnerForBackgroundDemo.java
+++ src/test/java/StepDefinitions/TestRunnerForBackgroundDemo.java
@@ -1,16 +1,24 @@
 package StepDefinitions;
 
 import org.junit.runner.RunWith;
+
+
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.Cucumber;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/Features/BackgroundDemo/BackgroundDemo.feature",
 glue= {"StepsForBackground"},
 monochrome=true,
-plugin={"pretty","junit:target/JUnitReports/report.xml",
-		"json:target/JSONReports/report.json",
-		"html:target/HtmlReports"}
+plugin={/*"pretty","junit:target/JUnitReports/report.xml",*/
+		"pretty",
+        "html:target/HtmlReports/report.html",
+        "json:target/JsonReports/report.json",
+        "junit:target/JUnitReports/report.xml",
+        "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm" }// Allure plugin
 )
 public class TestRunnerForBackgroundDemo {
 
